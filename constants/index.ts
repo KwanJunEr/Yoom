09a1455ab88f9fd280419985ea3c41ab6a3f2e96
Jonas Kwan <jonastheitguy@gmail.com>
2024-08-26 12:11:21 +0800

    Commit first

---
+++ constants/index.ts
@@ -0,0 +1,37 @@
+export const sidebarLinks = [
+    {
+        label:'Home',
+        route:'/',
+        imgUrl : '/icons/Home.svg',   
+    },
+    {
+        label:'Upcoming',
+        route:'/upcoming',
+        imgUrl : '/icons/upcoming.svg',   
+    },
+    {
+        label:'Previous',
+        route:'/previous',
+        imgUrl : '/icons/previous.svg',   
+    },
+    {
+        label:'Recordings',
+        route:'/recordings',
+        imgUrl : '/icons/Video.svg',   
+    },
+    {
+        label:'Personal Room',
+        route:'/personal-room',
+        imgUrl : '/icons/add-personal.svg',   
+    },
+    
+]
+
+export const avatarImages = [
+    '/images/avater-1.jpeg',
+    '/images/avater-2.jpeg',
+    '/images/avater-3.png',
+    '/images/avater-4.png',
+    '/images/avater-5.png',
+  ];
+  
